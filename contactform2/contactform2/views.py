d4d3330862429cb45021e0371929a6148ec577c6
RystemAsqar <bornqazaqi@gmail.com>
2022-03-06 23:41:16 +0600

    addingContactPagePart1

---
+++ contactform2/contactform2/views.py
@@ -0,0 +1,30 @@
+from unicodedata import name
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('full-name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+
+        data = {
+            'name': name,
+            'email': email, 
+            'subject': subject, 
+            'message': message
+        }
+
+        message = '''
+        New message: {}
+
+        From: {}
+        '''.format(data['message'],data['email'])
+        send_mail(data['subject'], message, '200103368@stu.sdu.edu.kz', ['bornqazaqi@gmail.com'])
+
+    return render(request, 'contactform2/index.html', {})
+    
