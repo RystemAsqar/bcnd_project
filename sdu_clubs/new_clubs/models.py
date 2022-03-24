6752863b25b5e507b23a4aac6925baab8fcbf23f
Nasii <200103249@stu.sdu.edu.kz>
2022-03-24 17:25:10 +0600

    pageAboutClubs

---
+++ sdu_clubs/new_clubs/models.py
@@ -0,0 +1,16 @@
+from django.utils import timezone
+from django.db import models
+
+
+# Create your models here.
+
+
+class Order(models.Model):
+    order_dt = models.DateTimeField(default=timezone.now)
+    order_name = models.CharField(max_length=200, verbose_name='Name')
+    order_phone = models.CharField(max_length=200, verbose_name='Phone')
+
+    def __str__(self):
+        return self.order_name
+
+
