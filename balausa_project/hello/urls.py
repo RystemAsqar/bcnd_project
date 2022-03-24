81bc7e60083c212feae90981b3af4b384fd7595e
RystemAsqar <90493498+RystemAsqar@users.noreply.github.com>
2022-03-24 18:07:09 +0600

    Delete balausa_project directory

--- balausa_project/hello/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import TaskList,TaskCreate
-
-urlpatterns = [
-    path('',TaskList.as_view(),name = 'tasks'),
-    path('task-create/' , TaskCreate.as_view(),name='task-create'),
-]
